0368a6383b8e598f5fba50b59d2be3aa64485334
GoranZec3 <111533731+GoranZec3@users.noreply.github.com>
2022-09-03 10:41:39 +0200

    Update potentiometer_leds.py

--- potentiometer_leds.py
+++ potentiometer_leds.py
@@ -13,32 +13,34 @@ iterator = util.Iterator(board)
 iterator.start()
 time.sleep(0.1)
 
+try:
+    while True:
 
-while True:
+        analog_value = pin.read()
+        #str_val = '%.3f' % (analog_value)      #converting input to string this was my first solution how to conver 'NoneType'
+        round_value = round(analog_value, 3)    #setting input to 3 decimals
+        value_as_float = float(round_value)     #converting round_value to float
+        print(value_as_float)
 
-    analog_value = pin.read()
-    #str_val = '%.3f' % (analog_value)      #converting input to string this was my first solution how to conver 'NoneType'
-    round_value = round(analog_value, 3)    #setting input to 3 decimals
-    value_as_float = float(round_value)     #converting round_value to float
-    print(value_as_float)
+        time.sleep(0.1)
 
-    time.sleep(0.1)
+        #Splitting potentiometer on 4 each quarter dirves one led
+        if value_as_float < 0.250:
+            led_light.write(value_as_float * 4)    #Setting value from 0 - 0.250 as 0-1
+            led_light2.write(0)
+            led_light3.write(0)
+            led_light4.write(0)
 
-    #Splitting potentiometer on 4 each quarter dirves one led
-    if value_as_float < 0.250:
-        led_light.write(value_as_float * 4)    #Setting value from 0 - 0.250 as 0-1
-        led_light2.write(0)
-        led_light3.write(0)
-        led_light4.write(0)
+        elif value_as_float >= 0.250 and value_as_float<= 0.500:
+            led_light2.write((value_as_float - 0.250) * 4)  #Setting value to 0 than value from 0.250 - 0.500 as 0-1
+            led_light3.write(0)
+            led_light4.write(0)
 
-    elif value_as_float >= 0.250 and value_as_float<= 0.500:
-        led_light2.write((value_as_float - 0.250) * 4)  #Setting value to 0 than value from 0.250 - 0.500 as 0-1
-        led_light3.write(0)
-        led_light4.write(0)
+        elif value_as_float >= 0.500 and value_as_float <= 0.750:
+            led_light3.write((value_as_float - 0.500) * 4)  #Setting value to 0 than value from 0.500 - 0.750 as 0-1
+            led_light4.write(0)
 
-    elif value_as_float >= 0.500 and value_as_float <= 0.750:
-        led_light3.write((value_as_float - 0.500) * 4)  #Setting value to 0 than value from 0.500 - 0.750 as 0-1
-        led_light4.write(0)
-
-    elif value_as_float >= 0.750:
-        led_light4.write((value_as_float - 0.750) * 4)  #Setting value to 0 than value from 0.750 - 1 as 0-1
+        elif value_as_float >= 0.750:
+            led_light4.write((value_as_float - 0.750) * 4)  #Setting value to 0 than value from 0.750 - 1 as 0-1
+except KeyboardInterrupt:
+    board.exit()
